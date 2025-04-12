b870ef379f68e430a9e593d0a0208af2c2c039ba
EmporiumBux <emporiumcontatos@gmail.com>
2025-04-12 15:28:36 -0300

    Atualização metadados

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,14 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'EmporiumBux',
+  description: 'Criado por EmporiumBux',
+  generator: 'EmporiumBux',
+  icons: {
+    icon: 'https://cdn.discordapp.com/avatars/1331052881171976204/32938fa5476a5265badcfaf5adffe436.png?size=2048',
+  },
 }
+
 
 export default function RootLayout({
   children,
